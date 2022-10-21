dd716731bcf1b6ee439604ec5cdfd06b26332c0c
Łukasz Rożek <lukasz.rozek@consileon.pl>
2022-10-21 10:01:12 +0200

    more containers

--- src/test/java/pl/lrozek/containers/ContainersTest.java
+++ src/test/java/pl/lrozek/containers/ContainersTest.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ContainersTest {
 
-    @ValueSource(strings = { "ibmcom/mq:9.2.4.0-r1", "mongo/6.0.2-focal" })
+    @ValueSource(strings = { "ibmcom/mq:9.2.4.0-r1", "mongo:5.0.9-focal", "mongo:6.0.2-focal", "gcr.io/distroless/java17:nonroot" })
     @ParameterizedTest
     public void itShouldStartContainer(String image) {
         // given
